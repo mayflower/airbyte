b08e76315a686497b360ab68366645080f14be87
kaidobit <patrick.shaikh@mayflower.de>
2025-03-24 16:16:47 +0100

    WIP impl

--- airbyte-integrations/connectors/source-langchain-docling-loader/unit_tests/test_streams.py
+++ airbyte-integrations/connectors/source-langchain-docling-loader/unit_tests/test_streams.py
@@ -6,19 +6,19 @@ from http import HTTPStatus
 from unittest.mock import MagicMock
 
 import pytest
-from source_langchain_docling_loader.source import LangchainDoclingLoaderStream
+from source_langchain_docling_loader.source import IncrementalFileLoaderStream
 
 
 @pytest.fixture
 def patch_base_class(mocker):
     # Mock abstract methods to enable instantiating abstract class
-    mocker.patch.object(LangchainDoclingLoaderStream, "path", "v0/example_endpoint")
-    mocker.patch.object(LangchainDoclingLoaderStream, "primary_key", "test_primary_key")
-    mocker.patch.object(LangchainDoclingLoaderStream, "__abstractmethods__", set())
+    mocker.patch.object(IncrementalFileLoaderStream, "path", "v0/example_endpoint")
+    mocker.patch.object(IncrementalFileLoaderStream, "primary_key", "test_primary_key")
+    mocker.patch.object(IncrementalFileLoaderStream, "__abstractmethods__", set())
 
 
 def test_request_params(patch_base_class):
-    stream = LangchainDoclingLoaderStream()
+    stream = IncrementalFileLoaderStream()
     # TODO: replace this with your input parameters
     inputs = {"stream_slice": None, "stream_state": None, "next_page_token": None}
     # TODO: replace this with your expected request parameters
@@ -27,7 +27,7 @@ def test_request_params(patch_base_class):
 
 
 def test_next_page_token(patch_base_class):
-    stream = LangchainDoclingLoaderStream()
+    stream = IncrementalFileLoaderStream()
     # TODO: replace this with your input parameters
     inputs = {"response": MagicMock()}
     # TODO: replace this with your expected next page token
@@ -36,7 +36,7 @@ def test_next_page_token(patch_base_class):
 
 
 def test_parse_response(patch_base_class):
-    stream = LangchainDoclingLoaderStream()
+    stream = IncrementalFileLoaderStream()
     # TODO: replace this with your input parameters
     inputs = {"response": MagicMock()}
     # TODO: replace this with your expected parced object
@@ -45,7 +45,7 @@ def test_parse_response(patch_base_class):
 
 
 def test_request_headers(patch_base_class):
-    stream = LangchainDoclingLoaderStream()
+    stream = IncrementalFileLoaderStream()
     # TODO: replace this with your input parameters
     inputs = {"stream_slice": None, "stream_state": None, "next_page_token": None}
     # TODO: replace this with your expected request headers
@@ -54,7 +54,7 @@ def test_request_headers(patch_base_class):
 
 
 def test_http_method(patch_base_class):
-    stream = LangchainDoclingLoaderStream()
+    stream = IncrementalFileLoaderStream()
     # TODO: replace this with your expected http request method
     expected_method = "GET"
     assert stream.http_method == expected_method
@@ -72,12 +72,12 @@ def test_http_method(patch_base_class):
 def test_should_retry(patch_base_class, http_status, should_retry):
     response_mock = MagicMock()
     response_mock.status_code = http_status
-    stream = LangchainDoclingLoaderStream()
+    stream = IncrementalFileLoaderStream()
     assert stream.should_retry(response_mock) == should_retry
 
 
 def test_backoff_time(patch_base_class):
     response_mock = MagicMock()
-    stream = LangchainDoclingLoaderStream()
+    stream = IncrementalFileLoaderStream()
     expected_backoff_time = None
     assert stream.backoff_time(response_mock) == expected_backoff_time
