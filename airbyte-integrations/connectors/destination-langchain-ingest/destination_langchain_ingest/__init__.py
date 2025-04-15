fe44d1bd40e937fe06f549ae7ab3bdb7f79a71a6
Alexander Günther <alexander.guenther@mayflower.de>
2025-04-15 17:06:56 +0200

    trying to remove accidental edits

--- airbyte-integrations/connectors/destination-langchain-ingest/destination_langchain_ingest/__init__.py
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (c) 2025 Airbyte, Inc., all rights reserved.
-#
-
-
-from .destination import DestinationLangchainIngest
-
-__all__ = ["DestinationLangchainIngest"]
--- airbyte-integrations/connectors/source-langchain-sitemaploader/main.py
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (c) 2025 Airbyte, Inc., all rights reserved.
-#
-
-from source_langchain_sitemaploader.run import run
-
-if __name__ == "__main__":
-    run()
