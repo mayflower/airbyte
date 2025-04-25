bffb25777727935a8e153ed9549b67d24ef384cd
Alexander Günther <alexander.guenther@mayflower.de>
2025-04-25 16:40:47 +0200

    initial working state

---
+++ airbyte-integrations/connectors/destination-mariadb-langchain/destination_mariadb_langchain/destination.py
@@ -0,0 +1,88 @@
+import logging
+from logging import Logger
+from typing import Any, Iterable, Mapping, Optional
+
+from airbyte.secrets import SecretString
+from airbyte.strategies import WriteStrategy
+from airbyte_cdk.destinations import Destination
+from airbyte_cdk.models import (
+    AirbyteConnectionStatus,
+    AirbyteMessage,
+    ConfiguredAirbyteCatalog,
+    ConnectorSpecification,
+    DestinationSyncMode,
+    Status,
+)
+
+
+logger = logging.getLogger("airbyte")
+
+from destination_mariadb_langchain import mariadb_processor
+from destination_mariadb_langchain.common.catalog.catalog_providers import CatalogProvider
+from destination_mariadb_langchain.config import ConfigModel
+
+
+class DestinationMariaDB(Destination):
+    sql_processor: mariadb_processor.MariaDBProcessor
+
+    def _init_sql_processor(self, config: ConfigModel, configured_catalog: Optional[ConfiguredAirbyteCatalog] = None) -> None:
+        self.sql_processor = mariadb_processor.MariaDBProcessor(
+            sql_config=mariadb_processor.DatabaseConfig(
+                host=config.indexing.host,
+                port=config.indexing.port,
+                database=config.indexing.database,
+                username=config.indexing.username,
+                password=SecretString(config.indexing.credentials.password),
+            ),
+            splitter_config=config.processing,
+            embedder_config=config.embedding,
+            catalog_provider=CatalogProvider(configured_catalog),
+        )
+
+    def write(
+        self,
+        config: Mapping[str, Any],
+        configured_catalog: ConfiguredAirbyteCatalog,
+        input_messages: Iterable[AirbyteMessage],
+    ) -> Iterable[AirbyteMessage]:
+        import pydevd_pycharm
+        pydevd_pycharm.settrace('192.168.178.27', port=55507, stdoutToServer=True, stderrToServer=True)
+        logger.info("Write starting up")
+        parsed_config = ConfigModel.parse_obj(config)
+        self._init_sql_processor(config=parsed_config, configured_catalog=configured_catalog)
+        try:
+            logger.info("Before the yield from call")
+            yield from self.sql_processor.process_airbyte_messages_as_generator(
+                input_messages=input_messages,
+                write_strategy=WriteStrategy.AUTO,
+            )
+        except Exception as e:
+            raise e
+
+        logger.info("After this, I should terminate")
+
+    def check(self, check_logger: Logger, config: Mapping[str, Any]) -> AirbyteConnectionStatus:
+        # import pydevd_pycharm
+        # pydevd_pycharm.settrace("192.168.178.27", port=55507, stdoutToServer=True, stderrToServer=True)
+
+        try:
+            parsed_config = ConfigModel.parse_obj(config)
+            self._init_sql_processor(config=parsed_config)
+            self.sql_processor.sql_config.get_sql_engine().connect()
+            check_logger.info("Connection successful")
+            return AirbyteConnectionStatus(status=Status.SUCCEEDED)
+        except Exception as e:
+            check_logger.error(f"An exception occurred: {repr(e)}")
+            return AirbyteConnectionStatus(status=Status.FAILED, message=f"An exception occurred: {repr(e)}")
+
+    def spec(self, *args: Any, **kwargs: Any) -> ConnectorSpecification:
+        return ConnectorSpecification(
+            documentationUrl="https://docs.airbyte.com/integrations/destinations/mariadb",  # TODO something
+            supportsIncremental=True,
+            supported_destination_sync_modes=[
+                DestinationSyncMode.overwrite,
+                DestinationSyncMode.append,
+                DestinationSyncMode.append_dedup,
+            ],
+            connectionSpecification=ConfigModel.schema(),  # type: ignore[attr-defined]
+        )
---
+++ airbyte-integrations/connectors/destination-mariadb-langchain/main.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2024 Airbyte, Inc., all rights reserved.
+#
+
+from destination_mariadb_langchain.run import run
+
+
+if __name__ == "__main__":
+    run()
